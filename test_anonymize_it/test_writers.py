6774710f7328a09dad5bdc95c6a0ca22ef200323
Michael Basnight <mbasnight@gmail.com>
2020-05-13 09:55:37 -0500

    Fix writing of multiple strings in a list (#21)

--- test_anonymize_it/test_writers.py
+++ test_anonymize_it/test_writers.py
@@ -8,16 +8,18 @@ def test_fswriter():
         "directory": "test_output"
     }
 
-    data = {
-        "test": "this is a test".split()
-    }
+    data = [
+        json.dumps({"this": "is the json header for a elasticsearch bulk"}),
+        json.dumps({"test": "this is a test".split()})
+    ]
 
     fswriter = writers.FSWriter(params) 
     file_name = "output"
-    fswriter.write_data(json.dumps(data), file_name=file_name)
+    fswriter.write_data(data, file_name=file_name)
     dir_path = os.path.join(os.path.abspath(os.getcwd()), fswriter.out_dir)
     with open(f'{dir_path}/{file_name}.json', 'r') as f:
-        in_data = json.loads(f.read())
+        # The list has one extra newline which splits to a new row, so we remove it
+        in_data = f.read().split("\n")[:-1]
 
     os.remove(f'{dir_path}/{file_name}.json')
 
